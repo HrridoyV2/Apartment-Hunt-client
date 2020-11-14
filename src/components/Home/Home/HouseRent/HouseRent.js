e6841677684507d3068c8d21d9d20bebf6c8d402
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-11-15 02:54:03 +0600

    s1

--- src/components/Home/Home/HouseRent/HouseRent.js
+++ src/components/Home/Home/HouseRent/HouseRent.js
@@ -4,7 +4,7 @@ const HouseRent = () => {
     return (
         <section>
             <div>
-                <div className="text-center my-5">
+                <div className="text-center my-5" style={{ color: '#16322E' }}>
                     <h5>House Rent</h5>
                     <h1>Discover the latest Rent
                     <br />
