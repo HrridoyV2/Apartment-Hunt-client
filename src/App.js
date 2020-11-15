b05755f53e6a445cb620175a0e297baf94a223dd
HrridoyV2 <hrridoyv2@gmail.com>
2020-11-15 17:37:12 +0600

    git git

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './components/Home/Home/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home/Home";
+import HuntDetails from "./components/Home/Home/HuntDetails/HuntDetails/HuntDetails";
 function App() {
   return (
-
     <Router>
       <Switch>
-        <Route>
+        <Route exact path="/">
           <Home></Home>
+        </Route>
+        <Route path="/home">
+          <HuntDetails />
         </Route>
       </Switch>
     </Router>
