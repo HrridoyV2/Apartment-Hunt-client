648fa72748bda1d1eac87e267081b583cbb3f295
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-11-16 00:15:08 +0600

    done home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+
 import Home from './components/Home/Home/Home';
 import HuntDetails from "./components/HuntDetails/HuntDetails/HuntDetails";
 
