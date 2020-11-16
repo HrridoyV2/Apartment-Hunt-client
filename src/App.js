0bf81a549b0ac782132c968de00fe86a9c721227
HrridoyV2 <hrridoyv2@gmail.com>
2020-11-16 20:58:58 +0600

    place order done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState, createContext } from 'react';
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home/Home";
-import HuntDetails from "./components/HuntDetails/HuntDetails/HuntDetails";
+import HomeDetails from './components/HomeDetails/HomeDetails/HomeDetails'
 import MyOrders from "./components/MyOrders/MyOrders";
 import Login from './components/Login/Login';
-
+import PrivateRoute from './components/Login/PrivateRoute';
 export const UserContext = createContext();
 
 function App() {
@@ -18,22 +18,25 @@ function App() {
   });
   return (
     <UserContext.Provider value={[user, setUser]}>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home></Home>
-        </Route>
-        <Route path="/home">
-          <HuntDetails></HuntDetails>
-        </Route>
-        <Route path='/myOrders'>
-          <MyOrders />
-        </Route>
-        <Route path='/login'>
-          <Login> </Login>
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <PrivateRoute path="/home/:id">
+            <HomeDetails></HomeDetails>
+          </PrivateRoute>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/myOrders">
+            <MyOrders />
+          </Route>
+          <Route path="/login">
+            <Login> </Login>
+          </Route>
+        </Switch>
+      </Router>
     </UserContext.Provider>
   );
 }
---
+++ src/components/HomeDetails/HomeDetails/HomeDetails.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Navbar from "../../../components/Home/Home/Navbar/Navbar";
+import Apartment from "../Apartment/Apartment";
+import BookingDetails from "../BookingDetails/BookingDetails";
+
+const HomeDetails = () => {
+    const {id} = useParams();
+    console.log(id);
+    const [selectedRent, setSelectedRent] = useState({});
+
+    useEffect(() => {
+      fetch("http://localhost:5000/home/"+id)
+        .then((res) => res.json())
+        .then((data) => {
+          setSelectedRent(data);
+        });
+    }, [id]);
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Apartment selectedRent={selectedRent}></Apartment>
+      <BookingDetails selectedRent={selectedRent}></BookingDetails>
+    </div>
+  );
+};
+
+export default HomeDetails;
